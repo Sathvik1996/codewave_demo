381c56df7fcc9c2aff975b5c876a45e201d57673
sathvik1996 <vsathvik1996@gmail.com>
2020-11-13 13:23:14 +0530

    the..database

--- codewave/settings.py
+++ codewave/settings.py
@@ -70,9 +70,8 @@ WSGI_APPLICATION = 'codewave.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = { 'default': dj_database_url.config(conn_max_age=600) }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
